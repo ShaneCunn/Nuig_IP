694caf06bb8ba3fb48d88ddfed1ba1cedcfc74c6
Shane Cunningham <shanecunningham@live.ie>
2016-03-24 12:42:06 +0000

    basic registration and meail is working

---
+++ registration/includes/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shane
+ * Date: 23/03/2016
+ * Time: 12:34
+ */
+
+
+define("DB_HOST" , "localhost");
+define("DB_USER" , "mydb2022o");
+define("DB_PASS" , "mydb2022o");
+define("DB_NAME" , "mydb2022");
+
+
---
+++ registration/myaccount.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shane
+ * Date: 23/03/2016
+ * Time: 12:32
+ */
+session_start();
+include('includes/config.php');
+include('includes/db.php');
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Account Details</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="./css/bootstrap.css" rel="stylesheet">
+    
+    <!-- Custom styles for this template -->
+    <link href="./css/starter-template.css" rel="stylesheet">
+
+   
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">Practice sign up project</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="index.php">Login</a></li>
+            <li><a href="register.php">Register</a></li>
+            <li><a href="logout.php">Logout</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <div class="container">
+        
+   <div class="jumbostron">
+
+          <h2>
+            Welcome <?php $_SESSION['user_name']  ?>
+          </h2>
+        </div>
+
+  
+     
+ 
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="./js/bootstrap.js"></script>
+ 
+  </body>
+</html>
