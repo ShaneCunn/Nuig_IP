a7d5199d569acedb4be7529edacb681a1c413aa9
Shane Cunningham <shanecunningham@live.ie>
2016-03-27 22:17:12 +0100

    nuig config file

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shane
+ * Date: 23/03/2016
+ * Time: 12:34
+ */
+
+
+define("DB_HOST" , "localhost");
+define("DB_USER" , "mydb2022cs");
+define("DB_PASS" , "ri5nij");
+define("DB_NAME" , "mydb2022");
+
+
+
+
+
